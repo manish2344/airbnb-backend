5311b3b7ae7e09c35b75113fd6348293d464769c
manish2344 <manishpanwar682@gmail.com>
2024-10-01 11:42:58 +0530

    Initial commit

--- airbnb_clone_backend/wsgi.py
+++ airbnb_clone_backend/wsgi.py
@@ -4,3 +4,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'airbnb_clone_backend.settings')
 
 application = get_wsgi_application()
+
+# Add this:
+app = application
